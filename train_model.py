138738b69d4dbc22a17c82890c887a7333f5a642
Dorus Keijzer <83274869+DorusKeijzer@users.noreply.github.com>
2024-03-17 15:26:00 +0100

    choice task 1

--- train_model.py
+++ train_model.py
@@ -9,8 +9,18 @@ import importlib.util
 from datetime import datetime
 
 
-def train(net: nn.Module, trainloader, criterion, optimizer):
-    for epoch in range(10):
+def train(net: nn.Module, trainloader, criterion, optimizer: torch.optim.Adam, decrease_learning_rate = False):
+    training_loss = []
+    eval_loss = []
+    for epoch in range(15):
+        # if deacrease_learning_rate is set to True:
+        if decrease_learning_rate and epoch > 0 and epoch % 5 == 0:
+            # halves learning rate every 5th epoch for choice task #1
+            for param_group in optimizer.param_groups:
+                lr = param_group['lr']
+                lr /= 2
+                param_group['lr'] = lr 
+            print(f"Halved learning rate to {lr}")
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
@@ -28,6 +38,7 @@ def train(net: nn.Module, trainloader, criterion, optimizer):
                 print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
+        training_loss.append(running_loss)
 
     print('Finished Training')
 
@@ -43,17 +54,22 @@ def load_model(model_filename):
 if __name__  == "__main__":
     if len(argv) != 2:
         raise Exception("Specify a model path")
-
+    
     model_filename = argv[1]
+    print(f"Loading {model_filename}...")
     model_module = load_model(model_filename)
-    model = model_module.model()  # Assuming your model class is named Model1
+    model = model_module.model()  
+    print(f"Succesfully loaded model {model.name} from {model_filename}")
+
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.adam(model.parameters(), lr= 0.001)
+    optimizer = optim.Adam(model.parameters(), lr= 0.001)
     
     from load_dataset import trainloader as trainloader
 
     train(model, trainloader, criterion, optimizer)
 
     now = datetime.now().strftime(r'%y%m%d_%H%M%S')
+    store_filepath = f"trained_models/{model.name}_{now}.pth"
 
-    torch.save(model.state_dict(), f"lenet_test{now}")
+    torch.save(model.state_dict(), store_filepath)
+    print(f"Saved model to {store_filepath}")
